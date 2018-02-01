1eba163ed0327197ff161426fa070415fa77e741
Staces <staescedric@gmail.com>
2018-02-01 12:04:03 +0100

    Init

---
+++ Assets/_Project/Scripts/DayAndNight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour {
+
+    public Light m_Sun;
+    [Range(0,10)]
+    public int m_Seconds = 1;
+    
+	
+	void FixedUpdate () {
+        m_Sun.transform.rotation = Quaternion.Euler(new Vector3(m_Sun.transform.rotation.x + Time.realtimeSinceStartup, 0f, 0f));
+	}
+}
