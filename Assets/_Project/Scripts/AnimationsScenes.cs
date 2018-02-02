3af78ec8694e74d5eed5b14a657bbb02991f77ff
Staces <staescedric@gmail.com>
2018-02-02 09:39:45 +0100

    Animation Robot's Head

---
+++ Assets/_Project/Scripts/AnimationsScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsScenes : MonoBehaviour {
+
+    public GameObject m_RobotNeck;
+
+    private void Awake()
+    {
+        m_neck = m_RobotNeck.GetComponent<Animator>();
+    }
+
+    void Update () {
+        if (!m_neck.GetBool("ActiveRobotHead") && Input.GetKeyDown(KeyCode.Space))
+            MoveRobotHead();
+	}
+
+    void MoveRobotHead()
+    {
+        m_neck.SetBool("ActiveRobotHead",true);
+    }
+
+    private Animator m_neck;
+}
