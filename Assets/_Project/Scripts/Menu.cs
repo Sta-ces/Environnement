9721023590357194979f5c58b25ff88ec8ca9be3
Staces <staescedric@gmail.com>
2018-02-02 13:03:30 +0100

    Menu

---
+++ Assets/_Project/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas m_Menu;
+
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            m_Menu.gameObject.SetActive(!m_Menu.gameObject.activeSelf);
+	}
+}
