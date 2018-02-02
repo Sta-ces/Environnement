e037c07f0601b715a52c6e0dd1f552544f240b41
Staces <staescedric@gmail.com>
2018-02-02 10:55:26 +0100

    Animation and Sounds

--- Assets/_Project/Scripts/AnimationsScenes.cs
+++ Assets/_Project/Scripts/AnimationsScenes.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationsScenes : MonoBehaviour {
 
     public GameObject m_RobotNeck;
     public GameObject m_RobotEyes;
+    public AudioSource m_SoundSuspence;
+    public Image m_BlackScreen;
 
     private void Awake()
     {
@@ -11,14 +15,23 @@ public class AnimationsScenes : MonoBehaviour {
     }
 
     void Update () {
-        if (!m_neck.GetBool("ActiveRobotHead") && Input.GetKeyDown(KeyCode.Space))
-            MoveRobotHead();
+        if (!m_neck.GetBool("ActiveRobotHead") && !m_SoundSuspence.isPlaying && Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine("MoveRobotHead");
 	}
 
-    void MoveRobotHead()
+    IEnumerator MoveRobotHead()
     {
+        m_SoundSuspence.Play();
+        yield return new WaitForSeconds(m_SoundSuspence.clip.length - 1);
         m_neck.SetBool("ActiveRobotHead",true);
         m_RobotEyes.SetActive(true);
+        StartCoroutine("DisplayBlackScreen");
+    }
+
+    IEnumerator DisplayBlackScreen()
+    {
+        yield return new WaitForSeconds(0.5f);
+        m_BlackScreen.gameObject.SetActive(true);
     }
 
     private Animator m_neck;
